7e99d363a5a559e7eca46495b585ef612c9b3edc
code <934258620@qq.com>
2019-04-17 17:10:38 +0800

    公司

--- application/web/controller/Index.php
+++ application/web/controller/Index.php
@@ -426,7 +426,7 @@ class Index extends Controller
         $this->assign('cateart', $cateart);
 
 
-        return view('cartdetail');
+        return view('');
     }
 
 
@@ -1300,8 +1300,7 @@ class Index extends Controller
     //用户中心
     public function member()
     {
-        //$mid  = $this->getMidByOpenid();
-        $mid  = 1;
+        $mid  = $this->getMidByOpenid();
         $member = Member::get(['id' => $mid]);
 
         $member['browse'] = MoneyLog::where('mid',$mid)->where('money','<',"0")->count();
