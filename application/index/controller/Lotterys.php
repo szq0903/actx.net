ef31ddfc3568ca539e09c742ad4681780d827565
code <934258620@qq.com>
2019-06-12 18:21:53 +0800

    公司

--- application/index/controller/Lotterys.php
+++ application/index/controller/Lotterys.php
@@ -3,7 +3,7 @@ namespace app\index\controller;
 use think\Controller;
 use think\Request;
 use app\index\model\Lottery;
-use app\index\model\LotteryPrize;
+use app\index\model\LotterySign;
 use app\index\model\LotteryLog;
 use lib\Form;
 
@@ -16,11 +16,54 @@ use lib\Form;
 class Lotterys extends Controller
 {
 	public $title='SEOCRM管理系统';
+    public $field=array();
+
 
 	public function _initialize()
 	{
 		check();
         $this->assign('menu', getLeftMenu());
+
+
+        $this->field = array(
+            array(
+                'itemname'=>'活动规则',
+                'fieldname'=>'rules_title',
+                'dtype'=>'text',
+                'vdefault'=>''
+            ),
+            array(
+                'itemname'=>'规则内容',
+                'fieldname'=>'rules_content',
+                'dtype'=>'htmltext',
+                'vdefault'=>''
+            ),
+            array(
+                'itemname'=>'兑奖信息',
+                'fieldname'=>'accept_title',
+                'dtype'=>'text',
+                'vdefault'=>''
+            ),
+            array(
+                'itemname'=>'兑奖内容',
+                'fieldname'=>'accept_content',
+                'dtype'=>'htmltext',
+                'vdefault'=>''
+            ),
+            array(
+                'itemname'=>'机构介绍',
+                'fieldname'=>'about_title',
+                'dtype'=>'text',
+                'vdefault'=>''
+            ),
+            array(
+                'itemname'=>'机构内容',
+                'fieldname'=>'about_content',
+                'dtype'=>'htmltext',
+                'vdefault'=>''
+            ),
+        );
+
 	}
 
 	/**
@@ -60,50 +103,60 @@ class Lotterys extends Controller
 	 * @return \think\mixed
 	 */
 	public function edit($id) {
-
-		$area= Area::get($id);
-
-		//判断抽奖是否存在
-		if(empty($area))
-		{
-			$this->error('要修改的抽奖不存在');
-		}
+        $temp = Lottery::get($id);
+        if(empty($temp))
+        {
+            $this->error('您要删除的抽奖不存在！');
+        }
 
 		//是否为提交表单
 		if (Request::instance()->isPost())
 		{
-			//抽奖名不能为空
-			if(!empty(Request::instance()->post('name')))
-			{
-				$area->name    	= Request::instance()->post('name');
-				$area->short_name    	= Request::instance()->post('short_name');
-				$area->longitude    	= Request::instance()->post('longitude');
-				$area->latitude    	= Request::instance()->post('latitude');
-				$area->level    	= Request::instance()->post('level');
-				$area->sort    	= Request::instance()->post('sort');
-				$area->parent_id = Request::instance()->post('parent_id');
-
-				$area->status = 1;
-				$area->save();
-				$this->success('修改成功！');
-			}else{
-				$this->error('抽奖名不能为空！');
-			}
+            $lottery = $temp;
+            $lottery->toppic    	= Request::instance()->post('toppic');
+            $lottery->title    	    = Request::instance()->post('title');
+            $lottery->startime    	= strtotime(Request::instance()->post('startime'));
+            $lottery->endtime    	= strtotime(Request::instance()->post('endtime'));
+            $lottery->rules_title    	= Request::instance()->post('rules_title');
+            $lottery->rules_content    	    = Request::instance()->post('rules_content');
+            $lottery->accept_title    	    = Request::instance()->post('accept_title');
+            $lottery->accept_content     = Request::instance()->post('accept_content');
+            $lottery->about_title    	    = Request::instance()->post('about_title');
+            $lottery->about_content     = Request::instance()->post('about_content');
+            $lottery->tel     = Request::instance()->post('tel');
+            $lottery->save();
+            $this->success('添加成功！');
 		}
 
-		//获取上级抽奖
-		$area1= Area::get($area->parent_id);
 
-		$this->assign('temp',$area1);
+        //处理时间
+        $temp['startime']=date('m/d/Y',$temp['startime']);
+        $temp['endtime']=date('m/d/Y',$temp['endtime']);
 
-		$this->assign('temp1',$area);
+        $temp['toppic'] = str_replace('\\','/',$temp['toppic']);
+        $temp['imagesize'] = filesize(getcwd().$temp['toppic']);
+		$this->assign('temp',$temp);
+
+
+        $form = new Form();
+        $formhtml = array();
+        foreach ($this->field as $val)
+        {
+            //开放链接
+            $val['islink'] = 1;
+            $val['vdefault'] = $temp[$val['fieldname']];
+            $arr['html'] = $form->fieldToForm($val,'form-control',$val['fieldname']);
+            $arr['fieldname'] = $val['fieldname'];
+            $arr['itemname'] = $val['itemname'];
+            $formhtml[] = $arr;
+        }
+        $this->assign('formhtml',$formhtml);
+
+
 		$this->assign('title','修改抽奖-'.$this->title);
 		$request = Request::instance();
 		$this->assign('act', $request->controller());
 
-		//为添加抽奖做准备
-		$this->assign('level',$area->level);
-		$this->assign('parent_id',$area->parent_id);
 
 		return $this->fetch();
 	}
@@ -136,51 +189,13 @@ class Lotterys extends Controller
                 $lottery->addtime = time();
                 $lottery->save();
 				$this->success('添加成功！');
-
 		}
 
-        $field = array(
-            array(
-                'itemname'=>'活动规则',
-                'fieldname'=>'rules_title',
-                'dtype'=>'text',
-                'vdefault'=>''
-            ),
-            array(
-                'itemname'=>'规则内容',
-                'fieldname'=>'rules_content',
-                'dtype'=>'htmltext',
-                'vdefault'=>''
-            ),
-            array(
-                'itemname'=>'兑奖信息',
-                'fieldname'=>'accept_title',
-                'dtype'=>'text',
-                'vdefault'=>''
-            ),
-            array(
-                'itemname'=>'兑奖内容',
-                'fieldname'=>'accept_content',
-                'dtype'=>'htmltext',
-                'vdefault'=>''
-            ),
-            array(
-                'itemname'=>'机构介绍',
-                'fieldname'=>'about_title',
-                'dtype'=>'text',
-                'vdefault'=>''
-            ),
-            array(
-                'itemname'=>'机构内容',
-                'fieldname'=>'about_content',
-                'dtype'=>'htmltext',
-                'vdefault'=>''
-            ),
-        );
+
 
         $form = new Form();
         $formhtml = array();
-        foreach ($field as $val)
+        foreach ($this->field as $val)
         {
             //开放链接
             $val['islink'] = 1;
@@ -197,13 +212,13 @@ class Lotterys extends Controller
 		$request = Request::instance();
 		$this->assign('act', $request->controller());
 
-		//为添加抽奖做准备
-		$this->assign('level',$level);
-		$this->assign('parent_id',$parent_id);
-
 
 		//获取上级抽奖信息
         $temp=array();
+
+        $temp['toppic']='/template/img/lottery/01.jpg';
+        $temp['imagesize'] = 876;
+
 		$this->assign('temp',$temp);
 
 		return $this->fetch('edit');
@@ -215,13 +230,13 @@ class Lotterys extends Controller
 	 * @return \think\mixed
 	 */
 	public function del($id) {
-		$area = Area::get($id);
-		if(empty($area))
+        $temp = Lottery::get($id);
+		if(empty($temp))
 		{
 			$this->error('您要删除的抽奖不存在！');
 		}else{
-			$area ->delete();
-			$this->success('删除抽奖成功！','index/areas/index');
+            $temp ->delete();
+			$this->success('删除抽奖成功！',url('index/lotterys/index'));
 		}
 		$this->assign('title','删除抽奖-'.$this->title);
 		$request = Request::instance();
@@ -248,5 +263,72 @@ class Lotterys extends Controller
         return view();
     }
 
+    public function cash($id)
+    {
+        $temp = LotteryLog::get($id);
+        if(empty($temp))
+        {
+            $this->error('您要编辑的中奖记录不存在！');
+        }
 
+        //是否为提交表单
+        if (Request::instance()->isPost())
+        {
+
+            $temp->status     = Request::instance()->post('status');
+            $temp->save();
+            $this->success('添加成功！');
+        }
+        $this->assign('temp', $temp);
+        return view();
+    }
+
+    public function signlist($lid)
+    {
+        $list = LotterySign::where('lid',$lid)->order('addtime')->paginate(10);
+
+        // 查询数据集
+        // 把数据赋值给模板变量list
+        $this->assign('list', $list);
+
+        $this->assign('lid', $lid);
+        //获取当当前控制器
+        $request = Request::instance();
+        $this->assign('act', $request->controller());
+        $this->assign('title','报名管理-'.$this->title);
+        return $this->fetch();
+    }
+
+    public function signedit($id)
+    {
+        $temp = LotterySign::get($id);
+        if(empty($temp))
+        {
+            $this->error('您要编辑的报名不存在！');
+        }
+        $this->assign('lid', $temp['lid']);
+        $this->assign('temp', $temp);
+
+        $this->assign('title','报名管理-'.$this->title);
+        $request = Request::instance();
+        $this->assign('act', $request->controller());
+        return view();
+    }
+
+
+    public function signdel($id) {
+        $temp = LotterySign::get($id);
+        if(empty($temp))
+        {
+            $this->error('您要删除的报名不存在！');
+        }else{
+            $lid = $temp['lid'];
+            $temp ->delete();
+            $this->success('删除报名成功！',url('index/lotterys/signlist',['lid'=>$lid]));
+        }
+        $this->assign('title','删除报名-'.$this->title);
+        $request = Request::instance();
+        $this->assign('act', $request->controller());
+        return $this->fetch();
+    }
 }
